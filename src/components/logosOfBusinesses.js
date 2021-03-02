b040c49d30394236fce5430de9a516501b5b4715
Jordy Yeoman <Jordyyeoman@gmail.com>
2021-03-02 12:27:32 +0800

    Second Commit

---
+++ src/components/logosOfBusinesses.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const LogosOfBusinesses = () => (
+    <section className="container px-4 py-24 mx-auto">
+  <h1 className="mb-12 text-sm font-bold tracking-wide text-center text-gray-800 uppercase">We’re proud to have played a part in these amazing journeys.</h1>
+  <div className="grid grid-cols-2 gap-10 text-center lg:grid-cols-8">
+    <div className="flex items-center justify-center">
+      <img src="/logos/todoist.svg" alt="Todoist Logo" className="block object-contain h-12" />
+    </div>
+    <div className="flex items-center justify-center">
+      <img src="/logos/slack-icon.svg" alt="Slack Logo" className="block object-contain h-12" />
+    </div>
+    <div className="flex items-center justify-center">
+      <img src="/logos/typeform.svg" alt="Typeform Logo" className="block object-contain h-12" />
+    </div>
+    <div className="flex items-center justify-center">
+      <img src="/logos/algolia.svg" alt="Algolia Logo" className="block object-contain h-12" />
+    </div>
+    <div className="flex items-center justify-center">
+      <img src="/logos/postcss.svg" alt="Postcss Logo" className="block object-contain h-12" />
+    </div>
+    <div className="flex items-center justify-center">
+      <img src="/logos/tailwindcss.svg" alt="TailwindCSS Logo" className="block object-contain h-12" />
+    </div>
+    <div className="flex items-center justify-center">
+      <img src="/logos/discord.svg" alt="Discord Logo" className="block object-contain h-12" />
+    </div>
+    <div className="flex items-center justify-center">
+      <img src="/logos/vimeo.svg" alt="Vimeo Logo" className="block object-contain h-12" />
+    </div>
+  </div>
+</section>
+)
+
+export default LogosOfBusinesses;
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,15 +1,19 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
+import AniLink from "gatsby-plugin-transition-link/AniLink"
 import SEO from "../components/seo"
 
 const SecondPage = () => (
   <Layout>
+    <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
     <SEO title="Page two" />
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <AniLink to="/" cover top="exit" direction="left" duration={1}>
+      <div className="link--general link--takiri">Takiri <span>Take me home</span></div>
+    </AniLink>
+    </div>
   </Layout>
 )
 
